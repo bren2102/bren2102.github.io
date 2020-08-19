98780c2ff74cd1b16ced236bba5f99313cb254ba
bren2102 <brb.yg.21@gmail.com>
2020-08-19 17:10:20 -0500

    Add toggle display

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import '../style/style.scss';
+
+document.querySelectorAll('.collapsible').forEach(button => {
+  button.addEventListener('click', () => {
+    const content = button.nextElementSibling;
+    console.log(content)
+    button.classList.toggle('collapsible--active');
+    
+    if(button.classList.contains('collapsible--active')){
+      content.style.maxHeight = content.scrollHeight + 'px';
+    } else {
+      content.style.maxHeight = 0;
+    }
+  })
+})
+
+(function () {
+  emailjs.init('user_O5FbAcZ71Jmx750DSsMLN');
+})();
+
+window.onload = function () {
+  document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    this.contact_number.value = Math.random() * 100000 | 0;
+    emailjs.sendForm('contact_service', 'contact_form', this);
+  });
+}
