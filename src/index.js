f48a5731f26ab34e7a3edbf67f989c29fd4cc56a
bren2102 <brb.yg.21@gmail.com>
2020-08-19 19:01:44 -0500

    add mobile and tablet view

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import '../style/style.scss';
 document.querySelectorAll('.collapsible').forEach(button => {
   button.addEventListener('click', () => {
     const content = button.nextElementSibling;
-    console.log(content)
     button.classList.toggle('collapsible--active');
     
     if(button.classList.contains('collapsible--active')){
       content.style.maxHeight = content.scrollHeight + 'px';
+      button.style.borderBottom = 'none';
+      button.lastElementChild.style.transform = "rotate(90deg)";
     } else {
       content.style.maxHeight = 0;
+      button.lastElementChild.style.transform = "rotate(0)";
+      button.style.borderBottom = '1px solid #DFE1E6';
     }
   })
 })
