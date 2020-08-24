9b24b1518c93e2b50b15c782ae123d31cdfb8f52
bren2102 <brb.yg.21@gmail.com>
2020-08-24 11:18:07 -0500

    Fix mobile and tablet views

--- src/index.js
+++ src/index.js
@@ -16,15 +16,3 @@ document.querySelectorAll('.collapsible').forEach(button => {
     }
   })
 })
-
-(function () {
-  emailjs.init('user_O5FbAcZ71Jmx750DSsMLN');
-})();
-
-window.onload = function () {
-  document.getElementById('contact-form').addEventListener('submit', function (event) {
-    event.preventDefault();
-    this.contact_number.value = Math.random() * 100000 | 0;
-    emailjs.sendForm('contact_service', 'contact_form', this);
-  });
-}
